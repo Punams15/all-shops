1bb67c134a822e90a3864c365f7372025495c156
Punams15 <keys2punam@gmail.com>
2025-09-16 21:36:07 -0500

    Fix lint issues

--- config/routes.rb
+++ config/routes.rb
@@ -14,24 +14,24 @@ Rails.application.routes.draw do
   get "categories/show"
   get "home/index"
   # Root path (homepage)
-  root "home#index"   #Products#index
+  root "home#index"   # Products#index
 
   # Categories → Subcategories → Products
-  resources :categories, only: [:index, :show] do
-    resources :subcategories, only: [:index, :show] do
-      resources :products, only: [:index]
+  resources :categories, only: [ :index, :show ] do
+    resources :subcategories, only: [ :index, :show ] do
+      resources :products, only: [ :index ]
     end
   end
 
   # Products and Sellers
-  resources :products, only: [:index, :show]
-  resources :sellers, only: [:index, :show]
+  resources :products, only: [ :index, :show ]
+  resources :sellers, only: [ :index, :show ]
 
   # Shopping cart & Orders
-  resources :cart, only: [:index]          # just a placeholder for now
-  resources :orders, only: [:index, :show]
+  resources :cart, only: [ :index ]          # just a placeholder for now
+  resources :orders, only: [ :index, :show ]
 
   # Promotions & Reviews
-  resources :promotions, only: [:index]
-  resources :reviews, only: [:index]
+  resources :promotions, only: [ :index ]
+  resources :reviews, only: [ :index ]
 end
